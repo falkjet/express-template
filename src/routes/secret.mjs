894b8eb8765e05295152362c4c727bdbdad422b5
Falk Markus Dursun Jetlund <falk@jetlund.com>
2022-04-19 12:24:58 +0200

    Finished secret page

--- src/routes/secret.mjs
+++ src/routes/secret.mjs
@@ -1,5 +1,12 @@
+import { layout } from "../layout.js";
+import html from "html-template-tag";
+
 export function get(req, res) {
-  res.html`
-        Secret
-    `;
+  if (!req.user) return res.redirect("/login");
+  res.html`$${layout(
+    { title: "Secret page", req },
+    html`
+      <p>This is a secret page. Only users who are logged in can read this.</p>
+    `
+  )}`;
 }
