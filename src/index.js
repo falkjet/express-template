b229f54e0bf6d812dcaf614a441aa7ea4033d8b6
Falk Markus Dursun Jetlund <falk@jetlund.com>
2022-04-19 11:51:18 +0200

    Added support for mjs and cjs routes

--- src/index.js
+++ src/index.js
@@ -9,18 +9,28 @@ async function main() {
   await mongoClient.connect();
 
   const promises = glob
-    .sync(join(__dirname, "routes/**/*.{jsx,js}"))
+    .sync(join(__dirname, "routes/**/*.{js,js,mjs,cjs}"))
     .map(async (path) => {
-      const mod = (await import(path)).default;
+      const mod = await import(path);
       path = join("/", relative(join(__dirname, "routes"), path));
       if (path.endsWith(".jsx")) path = path.substring(0, path.length - 4);
       if (path.endsWith(".js")) path = path.substring(0, path.length - 3);
+      if (path.endsWith(".mjs")) path = path.substring(0, path.length - 4);
+      if (path.endsWith(".cjs")) path = path.substring(0, path.length - 4);
       if (path.endsWith("index")) path = path.substring(0, path.length - 5);
 
-      for (let method of ["get", "post", "put", "patch", "delete"])
+      for (let method of ["get", "post", "put", "patch", "delete"]) {
         if (mod[method]) {
           app[method](path, ...(mod.middleware || []), mod[method].bind(mod));
         }
+        if (method in (mod.default || {})) {
+          app[method](
+            path,
+            ...(mod.default.middleware || []),
+            mod.default[method].bind(mod.default)
+          );
+        }
+      }
     });
 
   await Promise.all(promises);
---
+++ src/routes/secret.mjs
@@ -0,0 +1,5 @@
+export function get(req, res) {
+  res.html`
+        Secret
+    `;
+}
